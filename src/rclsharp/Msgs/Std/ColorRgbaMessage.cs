6e343f42b0cff64d6166fe6ef4a10494d3d93285
OJII3 <84656786+OJII3@users.noreply.github.com>
2026-04-19 19:07:11 +0900

    feat(msgs): std_msgs/ColorRGBA を追加

---
+++ src/rclsharp/Msgs/Std/ColorRgbaMessage.cs
@@ -0,0 +1,65 @@
+using Rclsharp.Cdr;
+
+namespace Rclsharp.Msgs.Std;
+
+/// <summary>
+/// std_msgs/msg/ColorRGBA の C# 表現。
+/// IDL:
+/// <code>
+/// float32 r
+/// float32 g
+/// float32 b
+/// float32 a
+/// </code>
+/// CDR 上は 4 バイト境界 × 4 = 16 バイト。
+/// </summary>
+public struct ColorRgbaMessage
+{
+    public const string RosTypeName = "std_msgs/msg/ColorRGBA";
+    public const string DdsTypeName = "std_msgs::msg::dds_::ColorRGBA_";
+
+    public float R;
+    public float G;
+    public float B;
+    public float A;
+
+    public ColorRgbaMessage(float r, float g, float b, float a)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public override string ToString() => $"ColorRGBA(r={R}, g={G}, b={B}, a={A})";
+}
+
+public sealed class ColorRgbaMessageSerializer : ICdrSerializer<ColorRgbaMessage>
+{
+    public static readonly ColorRgbaMessageSerializer Instance = new();
+
+    public bool IsKeyed => false;
+
+    public int GetSerializedSize(in ColorRgbaMessage value) => 16;
+
+    public void Serialize(ref CdrWriter writer, in ColorRgbaMessage value)
+    {
+        writer.WriteFloat(value.R);
+        writer.WriteFloat(value.G);
+        writer.WriteFloat(value.B);
+        writer.WriteFloat(value.A);
+    }
+
+    public void Deserialize(ref CdrReader reader, out ColorRgbaMessage value)
+    {
+        float r = reader.ReadFloat();
+        float g = reader.ReadFloat();
+        float b = reader.ReadFloat();
+        float a = reader.ReadFloat();
+        value = new ColorRgbaMessage(r, g, b, a);
+    }
+
+    public void SerializeKey(ref CdrWriter writer, in ColorRgbaMessage value)
+    {
+    }
+}
